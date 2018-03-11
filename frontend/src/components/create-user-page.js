d23e7a0b77f747f1d19af69f2279b31f0b92a820
Steven Branson <steve@diodeware.com>
2018-03-11 12:34:47 -0500

    Add styling for create user page

--- frontend/src/components/create-user-page.js
+++ frontend/src/components/create-user-page.js
@@ -6,11 +6,16 @@ export default function CreateUserPage(props) {
         return <Redirect to='/' />
     }
     return(
-        <div>
-            <h3>Create a new user</h3>
-            <input name="username" type="text" id="username-input" />
-            <input name="password" type="password" id="password-input" />
-            <button onClick={props.create}>Submit</button>
+        <div className="body">
+            <h3>Create a new account</h3>
+            <div className="spacer"></div>
+            <label htmlFor="username">User name</label>
+            <input className="login-input" name="username" type="text" id="username-input" />
+            <div className="spacer"></div>
+            <label htmlFor="username">Password</label>
+            <input className="login-input" name="password" type="password" id="password-input" />
+            <div className="spacer"></div>
+            <button id="create-submit-button" className="big-button" onClick={props.create}>Submit</button>
         </div>
     )
 }
