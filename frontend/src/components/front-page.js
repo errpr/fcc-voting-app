fe710ac09877c12e77b32348c3e361432abdbb17
Steven Branson <steve@diodeware.com>
2018-03-11 12:22:36 -0500

    Add styling for login page

--- frontend/src/components/front-page.js
+++ frontend/src/components/front-page.js
@@ -25,7 +25,7 @@ export default class FrontPage extends React.Component {
         }
         return(
             <div className="body">
-                <Link id="create-poll-button" to="/polls/create">Create a Poll</Link>
+                <Link className="big-button" id="create-poll-button" to="/polls/create">Create a Poll</Link>
                 <div className="spacer"></div>
                 <h1>Current Polls</h1>
                 <div className="hot-polls">
